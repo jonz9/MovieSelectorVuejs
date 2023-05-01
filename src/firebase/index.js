8372b4168c6bf1eb0c93b83e0fca3a8a216d57ac
John Zhang <zhangjohn.ca@gmail.com>
2023-05-01 17:44:08 -0400

    initialized firebase onto test branch: firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhZxyr-d3GbDfqr6obOP7mgQ6TL_z_8_0",
+  authDomain: "firstfirebase-b3640.firebaseapp.com",
+  projectId: "firstfirebase-b3640",
+  storageBucket: "firstfirebase-b3640.appspot.com",
+  messagingSenderId: "175383333398",
+  appId: "1:175383333398:web:0bfd136c2150e4b93a8337"
+};
+
+const app = initializeApp(firebaseConfig);
