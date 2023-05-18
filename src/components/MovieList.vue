46fda6772b679acb2246605d9760b95fdb613c90
John Zhang <zhangjohn.ca@gmail.com>
2023-05-18 00:39:20 -0400

    added all the html elements into MovieList.vue and attached vue directives to them

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,12 +4,22 @@ import axios from "axios";
 
 const APIkey = `e552c8258b3f71b20f3fc069ca964a73`;
 const movieData = ref(false);
+const trailerData = ref(false);
 const movieID = ref(null);
 
 const getTMDBData = async () => {
   movieData.value = (
-    await axios.get(`https://api.themoviedb.org/3/movie/${502356}?api_key=${APIkey}&language=en-US`)
+    await axios.get(`https://api.themoviedb.org/3/movie/${movieID.value}?api_key=${APIkey}&language=en-US`)
   ).data;
+
+  trailerData.value = (
+    await axios.get(
+      `https://api.themoviedb.org/3//movie/${movieID.value}/videos?api_key=${APIkey}&language=en-US&adult=false`
+    ).results.filter((trailer) => {
+      return trailer.type === "Trailer";
+    }
+    )
+  );
 };
 
 </script>
@@ -34,9 +44,24 @@ const getTMDBData = async () => {
     </form>
 
     <section id="movie-section">
-      <div></div>
+      <div v-if="movieData" class="poster">
+        <img :src="`https://image.tmdb.org/t/p/w500${movieData.poster_path}`" alt="poster">
+      </div>
+      <div v-if="movieData" class="movie-info">
+        <h1 id="title">{{ movieData.title }}</h1>
+        <br>
+        <h4 id="average-score">Average Score: {{ movieData.vote_average }}</h4>
+        <h4 id="popularity">Popularity: {{ movieData.vote_count }}</h4>
+        <h4 id="runtime">Runtime: {{ movieData.runtime }}</h4>
+        <h4 id="release-date">Release Date: {{ movieData.release_date }}</h4>
+        <h4 id="revenue">Revenue: {{ movieData.revenue }}</h4>
+        <h4 id="genres">Genre: {{ movieData.genres[0].name }}</h4>
+        <br>
+        <p id="overview">Synopsis: {{ movieData.overview }}</p>
+        <iframe v-if="trailerData" id="trailer" :src="`https://www.youtube.com/embed/${trailer.at(0).key}`" frameborder="0"></iframe>
+      </div>
     </section>
   </section>
 </template>
-
-<style scoped></style>
+<style scoped>
+</style>
